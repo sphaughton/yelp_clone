d79135b38265bd5323c51de5a90b83da341d7b04
Sean Haughton <seanphaughton@gmail.com>
2015-03-30 16:46:46 +0100

    Added error message if duplicate restaurant is entered

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Restaurant, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+end
+
+describe Restaurant, :type => :model do 
+  it 'is not valid with a name of less than three characters' do 
+    restaurant = Restaurant.new(name: "kf")
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
 end
