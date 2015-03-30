419a8895ac82fc61a0a2f1c5da74cb0a4f4a43c1
Sean Haughton <seanphaughton@gmail.com>
2015-03-30 16:15:31 +0100

    Added test to check reviews are also removed when a restaurant is deleted

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -13,5 +13,17 @@ feature 'reviewing' do
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
   end
-  
+
+  scenario 'allows reviews to be deleted when associated restaurant is deleted' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    click_link 'Delete KFC'
+
+    expect(current_path).to eq '/restaurants'
+    expect(page).to_not have_content('so so')
+  end
+
 end
