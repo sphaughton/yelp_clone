587e05b01f42e9f7a65a9f95db6df9bf59d0a3b9
Sean Haughton <seanphaughton@gmail.com>
2015-03-31 17:39:12 +0100

    Added working facebook authentication, testing user association in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
   root to: "restaurants#index"
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :restaurants do 
     resources :reviews
-  end
 
+    devise_scope :user do
+      get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    end
+  end
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+
+  before do 
+    visit('/')
+    click_link('Sign up')
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
+  end
+
   context 'no restaurants have been added' do 
     scenario 'should display a prompt to add a restaurant' do 
       visit '/restaurants'
@@ -23,8 +33,7 @@ feature 'restaurants' do
 
   context 'creating restaurants' do 
     scenario 'prompts user to fill out a form, then displays the new restaurant' do 
-      visit '/restaurants'
-      click_link 'Add a restaurant'
+      visit '/restaurants/new'
       fill_in 'Name', with: 'KFC'
       click_button 'Create Restaurant'
       expect(page).to have_content 'KFC'
