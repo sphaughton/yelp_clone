aba9a53ada74d007f44f9d45df3b68af9192cd7e
Sean Haughton <seanphaughton@gmail.com>
2015-05-05 12:03:29 +0100

    Added Review deletion unit test

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -3,4 +3,10 @@ require 'spec_helper'
 
 RSpec.describe ReviewsController, type: :controller do
 
+  it "can be deleted" do
+    review = Review.new
+    review.destroy
+    expect(review.id).to be nil
+  end
+
 end
